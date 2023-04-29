625db03668c58de9d0d63c0506a8523dc7396519
Jesús Benavente <benauca@gmail.com>
2023-04-29 17:20:52 +0000

    feat(test_camera_box): Refactor name

--- src/__test__/camera/test_camera_box.py
+++ src/__test__/camera/test_camera_box.py
@@ -1,24 +1,32 @@
-import pygame
+""" Test for camera box"""
+# Importamos los modulos necesarios
+
 import sys
 from random import randint
+import pygame
 
 
 class Tree(pygame.sprite.Sprite):
+    """Clase para crear los arboles"""
+
     def __init__(self, pos, group):
         super().__init__(group)
-        self.image = pygame.image.load('assets/tree.png').convert_alpha()
+        self.image = pygame.image.load("assets/tree.png").convert_alpha()
         self.rect = self.image.get_rect(topleft=pos)
 
 
 class Player(pygame.sprite.Sprite):
+    """Clase para crear el jugador"""
+
     def __init__(self, pos, group):
         super().__init__(group)
-        self.image = pygame.image.load('assets/player.png').convert_alpha()
+        self.image = pygame.image.load("assets/player.png").convert_alpha()
         self.rect = self.image.get_rect(center=pos)
         self.direction = pygame.math.Vector2()
         self.speed = 5
 
     def input(self):
+        """Gestionamos la entrada de teclado"""
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_UP]:
@@ -36,65 +44,81 @@ class Player(pygame.sprite.Sprite):
             self.direction.x = 0
 
     def update(self):
+        """Actualizamos la posicion del jugador en funcion de la direccion y la velocidad"""
         self.input()
         self.rect.center += self.direction * self.speed
 
 
 class CameraGroup(pygame.sprite.Group):
-    '''Funcionalidad de la camara. Ordenacion de elementos según eje y, y demás'''
+    """
+    Creamos una clase que hereda de pygame.sprite.Group para gestionar la camara.
+
+    Funcionalidad de la camara. Ordenacion de elementos según eje y, y demás
+    """
 
     def __init__(self) -> None:
         super().__init__()
-        # vamos a crear un dislay_surface donde dibujar los elementos y así no necesitar el elemento screen
+        # Vamos a crear un dislay_surface donde dibujar los elementos y
+        # un offset para gestionar la posicion de la camara
+        # y no tener que mover todos los elementos de la pantalla
+
         self.display_surface = pygame.display.get_surface()
 
-        # Offset asociado a la camara. Gestiona como se mueve la camara, que luego usaremos para pintar en los
-        # rectangulos de ground y de los sprites activos (Player and Tree)
+        # Offset asociado a la camara. Gestiona como se mueve la camara, que luego usaremos para
+        # pintar en los # rectangulos de ground y de los sprites activos (Player and Tree)
         self.offset = pygame.math.Vector2()
         # POsition en X. Mitad del tamaño de la imagen
         # Posiction en Y. Mitad del tamaño de la imagen
-        self.half_w = self.display_surface.get_size()[0]/2
-        self.half_y = self.display_surface.get_size()[1]/2
+        self.half_w = self.display_surface.get_size()[0] / 2
+        self.half_y = self.display_surface.get_size()[1] / 2
 
         # Camera box
-        self.camera_borders = { 'left': 200, 'right': 200, 'top': 100, 'bottom': 100}
-        l = self.camera_borders['left']
-        t = self.camera_borders['top']
-        w = self.display_surface.get_size()[0]  - (self.camera_borders['left'] + self.camera_borders['right'])
-        h = self.display_surface.get_size()[1] - (self.camera_borders['top'] + self.camera_borders['bottom'])
-		
-        self.camera_rect = pygame.Rect(l,t,w,h)
+        self.camera_borders = {"left": 200, "right": 200, "top": 100, "bottom": 100}
+        camera_left = self.camera_borders["left"]
+        camera_top = self.camera_borders["top"]
+        camera_weight = self.display_surface.get_size()[0] - (
+            self.camera_borders["left"] + self.camera_borders["right"]
+        )
+        camera_height = self.display_surface.get_size()[1] - (
+            self.camera_borders["top"] + self.camera_borders["bottom"]
+        )
 
-        self.ground_surf = pygame.image.load(
-            'assets/ground.png').convert_alpha()
+        self.camera_rect = pygame.Rect(
+            camera_left, camera_top, camera_weight, camera_height
+        )
+
+        self.ground_surf = pygame.image.load("assets/ground.png").convert_alpha()
         self.ground_rect = self.ground_surf.get_rect(topleft=(0, 0))
+
+    """ Centra la camara en el target."""
 
     def center_target_camera(self, target):
         self.offset.x = target.rect.centerx - self.half_w
         self.offset.y = target.rect.centery - self.half_y
 
-    def box_target_camera(self,target):
+    def box_target_camera(self, target):
 
         if target.rect.left < self.camera_rect.left:
-           self.camera_rect.left = target.rect.left
+            self.camera_rect.left = target.rect.left
         if target.rect.right > self.camera_rect.right:
-           self.camera_rect.right = target.rect.right
+            self.camera_rect.right = target.rect.right
         if target.rect.top < self.camera_rect.top:
-           self.camera_rect.top = target.rect.top
+            self.camera_rect.top = target.rect.top
         if target.rect.bottom > self.camera_rect.bottom:
-           self.camera_rect.bottom = target.rect.bottom
+            self.camera_rect.bottom = target.rect.bottom
 
-        self.offset.x = self.camera_rect.left - self.camera_borders['left']
-        self.offset.y = self.camera_rect.top - self.camera_borders['top']
+        self.offset.x = self.camera_rect.left - self.camera_borders["left"]
+        self.offset.y = self.camera_rect.top - self.camera_borders["top"]
 
     def custom_draw(self, player):
-        #self.center_target_camera(player)
+        # self.center_target_camera(player)
         self.box_target_camera(player)
         groud_offset_pos = self.ground_rect.topleft - self.offset
         self.display_surface.blit(self.ground_surf, groud_offset_pos)
 
-        # active elements
-        # Ordenamos los items en funcion del eje Y para que siempre esten visibiles los elementos con Mayor Y
+        # Active elements
+        # Ordenamos los items en funcion del eje Y para que siempre esten visibiles los elementos
+        # con Mayor Y
         for sprite in sorted(self.sprites(), key=lambda sprite: sprite.rect.centery):
 
             offset_pos = sprite.rect.topleft - self.offset
@@ -102,7 +126,10 @@ class CameraGroup(pygame.sprite.Group):
             # Pintaremos la imagen en funcion de la posicion de offset
             self.display_surface.blit(sprite.image, offset_pos)
 
-        pygame.draw.rect(self.display_surface, pygame.Color('yellow'), self.camera_rect, 5)
+        pygame.draw.rect(
+            self.display_surface, pygame.Color("yellow"), self.camera_rect, 5
+        )
+
 
 # Inicializamos el test
 pygame.init()
--- src/app.py
+++ src/app.py
@@ -7,34 +7,38 @@ from initScene import *
 from startScene import *
 from levelOneScene import *
 
+
 class App:
     """Create a single-window app with multiple scenes."""
-
 
     def __init__(self):
         """Initialize pygame and the application."""
         pygame.init()
         pygame.mixer.init()
-        self.screen = pygame.display.set_mode((Background.SCREEN_WIDTH, Background.SCREEN_HEIGHT))
+        self.screen = pygame.display.set_mode(
+            (Background.SCREEN_WIDTH, Background.SCREEN_HEIGHT)
+        )
         # Game loop
-        
+
         pygame.display.set_caption("Knightmare!")
-        pygame.display.set_icon(pygame.image.load(path.join(Background.img_dir,'player.png')).convert())
-        
+        pygame.display.set_icon(
+            pygame.image.load(path.join(Background.img_dir, "player.png")).convert()
+        )
+
         self.clock = pygame.time.Clock()
         self.currentScene = None
         self.scenes = {}
 
-    def run(self, initial_scene, fps = Background.FPS):
+    def run(self, initial_scene, fps=Background.FPS):
         """Run the main event loop."""
         self.currentScene = self.scenes[initial_scene]
         self.running = True
-        
+
         while self.running:
             pygame.time.get_ticks()
             # keep loop running at the right speed
             self.clock.tick(fps)
-        # Process input (events)
+            # Process input (events)
             events = pygame.event.get()
             for event in events:
                 if event.type == pygame.QUIT:
@@ -43,14 +47,14 @@ class App:
             self.currentScene.run()
             self.currentScene.draw(self.screen)
             self.changeScene(self.currentScene)
-            
+
             if self.running:
                 self.running = self.currentScene.running
-    
+
             pygame.display.flip()
 
-        #time.sleep(3)
-    
+        # time.sleep(3)
+
     def changeScene(self, currentScene):
         if currentScene.nextScene:
             if currentScene.toScene not in self.scenes:
@@ -58,12 +62,12 @@ class App:
             self.currentScene = self.scenes[currentScene.toScene]
 
     def addScene(self, sceneName):
-        className = sceneName + 'Scene'
+        className = sceneName + "Scene"
         sceneObj = globals()[className]
         self.scenes[sceneName] = sceneObj()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app = App()
-    app.addScene('Init')
-    app.run(initial_scene='Init')
+    app.addScene("Init")
+    app.run(initial_scene="Init")
